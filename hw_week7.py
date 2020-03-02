bac43432b30b5aea54a58b84919f0d84f829e3f8
Paul Wang <luck31101@hotmail.com.tw>
2020-03-02 10:33:36 +0800

    Update hw_week7.py
    submit  hw7

--- hw_week7.py
+++ hw_week7.py
@@ -1,8 +1,12 @@
 import random
 
 def hw_week7(secret_number):
-    # Your code here
-    pass
+  if int(secret_number) == 16:
+    print(f'Good job! You guessed my number !')
+  elif int(secret_number) > 16:
+    print('Your guess is too high.\nTake a guess.')
+  else:
+    print('Your guess is too low.\nTake a guess.')
 
 
 if __name__ == '__main__':
